c1c47553a118708ad5220f4d66e60ffa1915696f
Pouya Mobasher Behrouz <pooyamb@gmail.com>
2022-12-22 20:59:05 +0330

    Re-export parsers and de methods

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ pub mod parsers;
 
 #[cfg(feature = "serde")]
 pub mod de;
+
+pub use parsers::{BracketsQS, DelimiterQS, DuplicateQS, UrlEncodedQS};
+
+#[cfg(feature = "serde")]
+pub use de::{from_bytes, from_str};
