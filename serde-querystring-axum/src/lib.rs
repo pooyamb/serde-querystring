1bd2a26be2a86741691c0404e56f613f77763fbf
Pouya Mobasher Behrouz <pooyamb@gmail.com>
2023-01-04 14:27:55 +0330

    Add docs for axum extractor's config

--- serde-querystring-axum/src/lib.rs
+++ serde-querystring-axum/src/lib.rs
@@ -1,3 +1,6 @@
+use std::ops::Deref;
+use std::sync::Arc;
+
 use async_trait::async_trait;
 use axum_core::{
     extract::FromRequestParts,
@@ -5,8 +8,9 @@ use axum_core::{
 };
 use http::{request::Parts, StatusCode};
 use serde::de::DeserializeOwned;
-use serde_querystring::de::{Error, ParseMode};
-use std::ops::Deref;
+use serde_querystring::de::Error;
+
+pub use serde_querystring::de::ParseMode;
 
 /// Axum's Query extractor, modified to use serde-querystring.
 ///
@@ -45,6 +49,24 @@ use std::ops::Deref;
 /// If the query string cannot be parsed it will reject the request with a `422
 /// Unprocessable Entity` response.
 ///
+/// To change the default error and the parsing mode, add `QueryStringConfig` to your extensions.
+///
+/// ```rust,no_run
+/// use axum::{
+///     Router, Extension
+/// };
+/// use serde_querystring_axum::ParsingMode;
+///
+/// let app = Router::new().layer(Extension(
+///     QueryStringConfig::new().ehandler(|_err| {
+///         QueryStringError::new(
+///             StatusCode::BAD_GATEWAY,
+///             String::from("Something went wrong"),
+///         )
+///     }),
+/// ));
+/// ```
+///
 #[derive(Debug, Clone, Copy, Default)]
 pub struct QueryString<T>(pub T);
 
@@ -57,14 +79,20 @@ where
     type Rejection = QueryStringError;
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
-        let config = parts
+        let QueryStringConfig { mode, ehandler } = parts
             .extensions
             .get::<QueryStringConfig>()
             .cloned()
             .unwrap_or_default();
 
         let query = parts.uri.query().unwrap_or_default();
-        let value = serde_querystring::from_str(query, config.mode).map_err(QueryStringError)?;
+        let value = serde_querystring::from_str(query, mode).map_err(|e| {
+            if let Some(ehandler) = ehandler {
+                ehandler(e)
+            } else {
+                QueryStringError::default()
+            }
+        })?;
         Ok(QueryString(value))
     }
 }
@@ -80,33 +108,61 @@ impl<T> Deref for QueryString<T> {
 #[derive(Clone)]
 pub struct QueryStringConfig {
     mode: ParseMode,
+    ehandler: Option<Arc<dyn Fn(Error) -> QueryStringError + Send + Sync>>,
 }
 
 impl Default for QueryStringConfig {
     fn default() -> Self {
         Self {
             mode: ParseMode::Duplicate,
+            ehandler: None,
         }
     }
 }
 
 impl QueryStringConfig {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
     pub fn mode(mut self, mode: ParseMode) -> Self {
         self.mode = mode;
+        self
+    }
+
+    pub fn ehandler<F>(mut self, ehandler: F) -> Self
+    where
+        F: Fn(Error) -> QueryStringError + Send + Sync + 'static,
+    {
+        self.ehandler = Some(Arc::new(ehandler));
         self
     }
 }
 
 #[derive(Debug)]
-pub struct QueryStringError(Error);
+pub struct QueryStringError {
+    status: StatusCode,
+    body: String,
+}
+
+impl Default for QueryStringError {
+    fn default() -> Self {
+        Self {
+            status: StatusCode::BAD_REQUEST,
+            body: String::from("Failed to deserialize query string"),
+        }
+    }
+}
+
+impl QueryStringError {
+    pub fn new(status: StatusCode, body: String) -> Self {
+        Self { status, body }
+    }
+}
 
 impl IntoResponse for QueryStringError {
     fn into_response(self) -> Response {
-        (
-            StatusCode::BAD_REQUEST,
-            "Failed to deserialize query string",
-        )
-            .into_response()
+        (self.status, self.body).into_response()
     }
 }
 
@@ -240,6 +296,41 @@ mod tests {
         assert_eq!(
             body.data().await.unwrap().unwrap(),
             "Failed to deserialize query string"
-        )
+        );
+    }
+
+    #[tokio::test]
+    async fn correct_rejection_custom() {
+        #[derive(Deserialize)]
+        #[allow(dead_code)]
+        struct Params {
+            n: i32,
+        }
+
+        async fn handler(_: QueryString<Params>) {}
+
+        let app = Router::new().route("/", get(handler)).layer(Extension(
+            QueryStringConfig::new().ehandler(|_err| {
+                QueryStringError::new(
+                    StatusCode::BAD_GATEWAY,
+                    String::from("Something went wrong"),
+                )
+            }),
+        ));
+
+        let res = app
+            .oneshot(
+                Request::builder()
+                    .uri("/?n=string")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        let (parts, mut body) = res.into_parts();
+
+        assert_eq!(parts.status, StatusCode::BAD_GATEWAY);
+        assert_eq!(body.data().await.unwrap().unwrap(), "Something went wrong");
     }
 }
